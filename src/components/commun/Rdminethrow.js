77f74e3372bfb677ceeaf3703227e4e5471a2bb4
felixivanildo <83842694+felixivanildo@users.noreply.github.com>
2023-06-05 21:49:06 -0300

    Update Rdminethrow.js

--- src/components/commun/Rdminethrow.js
+++ src/components/commun/Rdminethrow.js
@@ -77,7 +77,7 @@ export async function Throw (data, project, image){
    
 
     
-    axios.post("http://localhost:3333/api/v1/cabeleira",     
+    axios.post("http://localhost:3333/api/v1/postissue",     
               { "issue": {
                 "subject": `${predio !== false ? predio : ""} ${setor !== false? setor : " "} ${project.project_name}- ${nome !== false? nome : ""} - ${matricula !== false? matricula : ""}`,
                 "project_id": `${project.project}`,
@@ -101,24 +101,4 @@ export async function Throw (data, project, image){
    
     
 
-//    axios.post('https://redmine.casal.al.gov.br/issues.json?key=ca239e06a3f10435c63c610ebd1809737ef72dd8', {
-//         "issue": {
-//           "subject": `Gesup Cadastro de Serviço - ${data.formtype[1].tipo} - ${data.formtype[3].tipo}`,
-//           "project_id": `${project.project}`,
-//           "tracker_id": `${project.tracker}`,
-//           "status_id": "1",
-//           "priority_id": "1",
-//           "recipient_email": `${data.formtype[2].tipo}`,
-//           "uploads": "",
-//           "description": `*Empregado(a):*  ${data.formtype[1].tipo}\r\n*Matrícula:* ${data.formtype[3].tipo}\r\n*E-mail:* ${data.formtype[2].tipo}\r\n*Telefone:* ${data.formtype[4].tipo}\r\n*Setor:* ${data.formtype[5].tipo}\r\n*Prédio:* ${data.formtype[6].tipo}\r\n*Tombamento:* ${data.formtype[7].tipo}\r\n*Descrição:* ${data.formtype[8].tipo}\r\n\r\nEste e-mail foi enviado de um formulário de contato em Intranet.`
-//         },
-//         "custom_fields": [{
-//           "value": `${data.formtype[7].tipo}`,
-//           "id": "37"
-//         },
-//         {
-//             "value": `${data.formtype[4].tipo}`,
-//             "id": "116"
-//         }]
-//       }).then(()=>{console.log('sucess')})
 }
